fe036bbfec0a8a880d5dd2fa0a7c44732d377d41
KimDonghyeon <ms0130k@unvus.com>
2023-11-29 07:13:43 +0900

    init

---
+++ src/pages/ErrorNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        404 {{ pageName }}
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        Oops. Nothing here...
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="blue"
+        unelevated
+        to="/"
+        label="Go Home"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {computed, defineComponent} from 'vue';
+import {useRoute} from "vue-router";
+let route = useRoute();
+const pageName = computed(() => route.params.catchAll);
+</script>
